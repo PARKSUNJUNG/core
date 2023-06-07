f3472eb130606c317832a1c1d03650c95bb6a144
PARKSUNJUNG <tjswjd094@gmail.com>
2023-06-07 23:39:25 +0900

    관심사의 분리

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -22,7 +22,7 @@ class RateDiscountPolicyTest {
         // then
         Assertions.assertThat(discount).isEqualTo(1000);
     }
-
+    /*
     @Test
     @DisplayName("VIP는 10% 할인이 적용되지 않아야 한다.")
     void vip_x () {
@@ -33,4 +33,5 @@ class RateDiscountPolicyTest {
         // then
         Assertions.assertThat(discount).isEqualTo(1000);
     }
+    */
 }
